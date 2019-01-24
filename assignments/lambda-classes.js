8a04a3dfebd4efdcfeef6a3c4a239c7bd67c6703
Tabita Filimon <tabitafilimon@gmail.com>
2019-01-24 16:19:26 -0500

    instructor class complete

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -11,3 +11,18 @@ class Person {
         return (`Hello, my name is ${this.name}, I am from ${this.location}`);
     }
 }
+
+class Instructor extends Person {
+    constructor(iProps){
+        super(iProps);
+        this.specialty = iProps.specialty;
+        this.favLanguage = iProps.favLanguage;
+        this.catchPhrase = iProps.catchPhrase;
+    }
+    demo(subject){
+        return ('Today we are learning about {subject}');
+    }
+    grade(student, subject){
+        return ('{student.name} receives a perfect score on {subject}');
+    }
+}
